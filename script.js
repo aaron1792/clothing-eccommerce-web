0e75b565e6c912e25ed72aee599ec68d6b2134e5
aaron1792 <aaron1792@gmail.com>
2024-02-09 13:24:08 +0000

    commit 7

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const triggerClose= document.querySelectorAll('[close-button]');
 const overlay= document.querySelector('[data-overlay]');
 
 for(let i = 0 ; i < triggerOpen.length; i++){
-let currentId = triggerOpen[i].dataset.target
+let currentId = triggerOpen[i].dataset.target,
 targetEl = document.querySelector(`#${currentId}`)
 const openData = function(){
 
@@ -34,3 +34,84 @@ if(this.closest('.has-child').classList != 'active'){
 }
 
 }))
+
+
+//Sorter
+const sorter = document.querySelector('.sort-list');
+if (sorter) {
+
+    const sortLi = sorter.querySelectorAll('li');
+    sorter.querySelector('.opt-trigger').addEventListener('click',function(){
+
+        sorter.querySelector('ul').classList.toggle('show')
+    });
+
+    sortLi.forEach((item) => item.addEventListener('click', function(){
+
+        sortLi.forEach((li) => li != this ? li.classList.remove('active'):null);
+        this.classList.add('active');
+        sorter.querySelector('.opt-trigger span.value').textContent = this.textContent;
+        sorter.querySelector('ul').classList.toggle('show');
+    }))
+}
+
+
+//Slider
+
+const swiper = new Swiper('.sliderbox', {
+
+    loop: true,
+    effect: 'fade',
+    autoHeight:true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable:true,
+    },
+  
+
+  });
+
+
+  //Carausel
+
+  const carousel = new Swiper('.carouselbox', {
+
+   
+ spaceBetween: 30,
+ slidesPerView: 'auto',
+ centeredSlides: true,
+  
+    // If we need pagination
+    navigation: {
+     nextEl:'.swiper-button-next',
+     prevEl:'.swiper-button-prev',
+    },
+    breakpoints:{
+
+        481:{
+            slidesPerView: 2,
+            slidesPerGroup: 1,
+            centeredSlides: false,
+
+
+        },
+        640:{
+            slidesPerView: 3,
+            slidesPerGroup: 3,
+            centeredSlides: false,
+
+
+        },
+        992:{
+            slidesPerView: 4,
+            slidesPerGroup: 4,
+            centeredSlides: false,
+
+
+        }
+    }
+  
+
+  });
